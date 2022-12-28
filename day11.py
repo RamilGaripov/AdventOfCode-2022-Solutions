36fff31298ddb16ae3f78e30ff360427ef2c855d
Ramil Garipov <garipovramil93@hotmail.ca>
2022-12-28 15:20:56 -0800

    Improved the solution for day 11, allowing both parts to be solved in a single run.

--- day11.py
+++ day11.py
@@ -1,3 +1,4 @@
+import copy
 import re
 from functools import reduce
 
@@ -41,7 +42,7 @@ def get_monkeys_and_conditions(data):
     return mon_list
 
 
-def inspect(init_worry, monkey, monkeys):
+def inspect(init_worry, monkey, monkeys, large_num):
     crt = reduce(lambda acc, monk: acc*monk.test, monkeys, 1)
     second_num = monkey.operation.split(' ')[1]
     if 'old' in second_num:
@@ -51,7 +52,7 @@ def inspect(init_worry, monkey, monkeys):
         new_worry_level = init_worry + second_num
     else:
         new_worry_level = init_worry * second_num
-    if num_is_large:
+    if large_num:
         return new_worry_level % crt
     return int(new_worry_level / 3)
 
@@ -67,34 +68,15 @@ def throw_item(src_monkey, target_monkey):
     target_monkey.items.append(src_monkey.items.pop(0))
 
 
-def monkey_business(all_monkeys):
+def monkey_business(all_monkeys, large_num):
     for monkey in all_monkeys:
         for index in range(len(monkey.items)):
-            monkey.items[0] = inspect(monkey.items[0], monkey, all_monkeys)
+            monkey.items[0] = inspect(monkey.items[0], monkey, all_monkeys, large_num)
             target = choose_target(monkey.items[0], monkey.test, monkey.action)
             throw_item(monkey, all_monkeys[target])
 
 
-if "__main__" == __name__:
-    with open("day11_input.txt") as file:
-        monkeys_data = [i for i in file.read().strip().split("\n\n")]
-    monkeys = get_monkeys_and_conditions(monkeys_data)
-    for mon in monkeys:
-        mon.print_properties()
-
-    num_is_large = True
-    if num_is_large:
-        for game_round in range(10000):
-            print(game_round)
-            monkey_business(monkeys)
-    else:
-        for game_round in range(20):
-            print(game_round)
-            monkey_business(monkeys)
-
-    for mon in monkeys:
-        mon.print_properties()
-
+def print_answer(monkeys, large_num):
     max1, max2 = 0, 0
     for monkey in monkeys:
         if max1 < monkey.activity:
@@ -102,5 +84,32 @@ if "__main__" == __name__:
             max1 = monkey.activity
         elif max2 < monkey.activity:
             max2 = monkey.activity
-    print("Most active monkeys have activities of", max1, "and", max2)
+    if large_num:
+        print("The answer to part 2:")
+    else:
+        print("The answer to part 1:")
+    print("The two most active monkeys have activities of", max1, "and", max2)
     print("Their combined monkey business is", max1 * max2)
+
+
+def solve(num_rounds, monkeys):
+    if num_rounds == 20:
+        large_numbers = False
+    else:
+        large_numbers = True
+    for game_round in range(num_rounds):
+        monkey_business(monkeys, large_numbers)
+
+    print_answer(monkeys, large_numbers)
+
+
+if "__main__" == __name__:
+    with open("day11_input.txt") as file:
+        monkeys_data = [i for i in file.read().strip().split("\n\n")]
+    monkeys_part_1 = get_monkeys_and_conditions(monkeys_data)
+    monkeys_part_2 = copy.deepcopy(monkeys_part_1)
+    solve(20, monkeys_part_1)
+    solve(10000, monkeys_part_2)
+
+
+
